b6610453fdb321969a53ca25a3d515f703127820
ILIA <ih13843251@gmail.com>
2025-02-12 10:26:26 -0800

    Dragon graphics

--- snake.py
+++ snake.py
@@ -21,11 +21,69 @@ class SNAKE:
         self.body = [pygame.math.Vector2(5,10),pygame.math.Vector2(4,10),pygame.math.Vector2(3,10)]
         self.direction = pygame.math.Vector2(1,0)
         self.eating = False
+
+        self.head_up = pygame.image.load('graphics/head_up.png').convert_alpha()
+        self.head_down = pygame.image.load('graphics/head_down.png').convert_alpha()
+        self.head_right = pygame.image.load('graphics/head_right.png').convert_alpha()
+        self.head_left = pygame.image.load('graphics/head_left.png').convert_alpha()
+		
+        self.tail_up = pygame.image.load('graphics/tail_up.png').convert_alpha()
+        self.tail_down = pygame.image.load('graphics/tail_down.png').convert_alpha()
+        self.tail_right = pygame.image.load('graphics/tail_right.png').convert_alpha()
+        self.tail_left = pygame.image.load('graphics/tail_left.png').convert_alpha()
+
+        self.body_vertical = pygame.image.load('graphics/body_vertical.png').convert_alpha()
+        self.body_horizontal = pygame.image.load('graphics/body_horizontal.png').convert_alpha()
+
+        self.body_tr = pygame.image.load('graphics/body_tr.png').convert_alpha()
+        self.body_tl = pygame.image.load('graphics/body_tl.png').convert_alpha()
+        self.body_br = pygame.image.load('graphics/body_br.png').convert_alpha()
+        self.body_bl = pygame.image.load('graphics/body_bl.png').convert_alpha()
     
     def draw_snake(self):
-        for bead in self.body:
-            bead_rect = pygame.Rect(bead.x*cell_size,bead.y*cell_size,cell_size,cell_size)
-            pygame.draw.rect(screen,(183,191,122),bead_rect)
+        self.update_head_graphics()
+        self.update_tail_graphics()
+
+        for index,block in enumerate(self.body):
+            x_pos = int(block.x * cell_size)
+            y_pos = int(block.y * cell_size)
+            block_rect = pygame.Rect(x_pos,y_pos,cell_size,cell_size)
+
+            if index == 0:
+                screen.blit(self.head,block_rect)
+            elif index == len(self.body) - 1:
+                screen.blit(self.tail,block_rect)
+            else:
+                previous_block = self.body[index + 1] - block
+                next_block = self.body[index - 1] - block
+                if previous_block.x == next_block.x:
+                    screen.blit(self.body_vertical,block_rect)
+                elif previous_block.y == next_block.y:
+                    screen.blit(self.body_horizontal,block_rect)
+                else:
+                    if previous_block.x == -1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == -1:
+                        screen.blit(self.body_tl,block_rect)
+                    elif previous_block.x == -1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == -1:
+                        screen.blit(self.body_bl,block_rect)
+                    elif previous_block.x == 1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == 1:
+                        screen.blit(self.body_tr,block_rect)
+                    elif previous_block.x == 1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == 1:
+                        screen.blit(self.body_br,block_rect)
+
+    def update_head_graphics(self):
+        head_relation = self.body[1] - self.body[0]
+        if head_relation == pygame.math.Vector2(1,0): self.head = self.head_left
+        elif head_relation == pygame.math.Vector2(-1,0): self.head = self.head_right
+        elif head_relation == pygame.math.Vector2(0,1): self.head = self.head_up
+        elif head_relation == pygame.math.Vector2(0,-1): self.head = self.head_down
+
+    def update_tail_graphics(self):
+        tail_relation = self.body[-2] - self.body[-1]
+        if tail_relation == pygame.math.Vector2(1,0): self.tail = self.tail_left
+        elif tail_relation == pygame.math.Vector2(-1,0): self.tail = self.tail_right
+        elif tail_relation == pygame.math.Vector2(0,1): self.tail = self.tail_up
+        elif tail_relation == pygame.math.Vector2(0,-1): self.tail = self.tail_down
+
 
     def move_snake(self):
         if self.eating == True:
@@ -84,7 +142,7 @@ clock =pygame.time.Clock()
 main_game = LOGIC()
 screen_update = pygame.USEREVENT
 pygame.time.set_timer(screen_update,200)
-load_circle = pygame.image.load('spinning circle.png').convert_alpha()
+load_circle = pygame.image.load('graphics/spinning circle.png').convert_alpha()
 angle = 0 
 rotation_speed = 6
 
@@ -107,7 +165,7 @@ while True:
 
     angle = (angle + rotation_speed)
 
-    screen.fill((175,50,70))
+    screen.fill((175,215,70))
     main_game.draw_stuff()
     pygame.display.update()
     clock.tick(60)
